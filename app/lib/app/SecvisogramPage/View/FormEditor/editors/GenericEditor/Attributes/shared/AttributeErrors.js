ddcfc8f0f15cfd7eecf5d443bc8492ec04604c23
Dominik Burgdörfer <dominik.burgdoerfer@exxcellent.de>
2025-05-02 08:46:06 +0200

    WIP: truncate long error message with ellipsis

--- app/lib/app/SecvisogramPage/View/FormEditor/editors/GenericEditor/Attributes/shared/AttributeErrors.js
+++ app/lib/app/SecvisogramPage/View/FormEditor/editors/GenericEditor/Attributes/shared/AttributeErrors.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
 import { getErrorTextColor } from '../../../GenericEditor.js'
 
 /**
@@ -12,20 +12,35 @@ export default function AttributeErrors({ validationErrors }) {
   return validationErrors.length ? (
     <div className="m-1">
       <ul className="block list-disc list-inside">
-        {validationErrors.map((e, i) => (
-          <li key={`${i}-${e.message}`} className="flex">
-            <div className="grid place-items-center px-2">
-              <FontAwesomeIcon
-                icon={faCircle}
-                className={getErrorTextColor([e])}
-                size="xs"
-              />
-            </div>
-            {e.message
-              ? e.message.charAt(0).toUpperCase() + e.message.slice(1)
-              : 'error message'}
-          </li>
-        ))}
+        {validationErrors.map((e, i) => {
+          const msg = e.message
+            ? e.message.charAt(0).toUpperCase() + e.message.slice(1)
+            : 'error message'
+
+          return (
+            <li key={`${i}-${e.message}`} className="flex">
+              <div className="grid place-items-center px-2">
+                <FontAwesomeIcon
+                  icon={faCircle}
+                  className={getErrorTextColor([e])}
+                  size="xs"
+                />
+              </div>
+              <span
+                className="inline-block"
+                style={{
+                  maxWidth: 300,
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  whiteSpace: 'nowrap',
+                }}
+                title={msg}
+              >
+                {msg}
+              </span>
+            </li>
+          )
+        })}
       </ul>
     </div>
   ) : null
