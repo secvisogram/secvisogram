e66b2b032c478b986ff2028ea2b7fc1b8e26e4f1
Dominik Burgdörfer <dominik.burgdoerfer@exxcellent.de>
2026-04-21 17:06:48 +0200

    WIP

--- app/lib/app/SecvisogramPage/View/FormEditor/editors/GenericEditor/Attributes/DropdownAttribute.js
+++ app/lib/app/SecvisogramPage/View/FormEditor/editors/GenericEditor/Attributes/DropdownAttribute.js
@@ -6,23 +6,34 @@ import DocumentEditorContext from '../../../../shared/DocumentEditorContext.js'
 import Attribute from './shared/Attribute.js'
 
 /**
- * @param {{
- *  label: string
- *  description: string
- *  options: string[]
- *  isEnum: boolean
- *  instancePath: string[]
- *  value: unknown
- *  property: import('../../../shared/types').Property
- *  disabled: boolean
- *  disableClearable: boolean
- *  renderOption?: React.ComponentProps<typeof Autocomplete>['renderOption']
- *  forcePopupIcon?: React.ComponentProps<typeof Autocomplete<string>>['forcePopupIcon']
- *  noOptionsText?: React.ComponentProps<typeof Autocomplete<string>>['noOptionsText']
- *  renderInput?: React.ComponentProps<typeof Autocomplete<string>>['renderInput']
- *  className?: React.ComponentProps<typeof Autocomplete<string>>['className']
- *  fillDefaultFunction?: () => void
- * }} props
+ * @param {object} props
+ * @param {string} props.label
+ * @param {string} props.description
+ * @param {any[]} props.options
+ * @param {boolean} props.isEnum
+ * @param {string[]} props.instancePath
+ * @param {unknown} props.value
+ * @param {import('../../../shared/types').Property} props.property
+ * @param {boolean} props.disabled
+ * @param {boolean} props.disableClearable
+ * @param {(option: any) => string} [props.getOptionValue]
+ * @param {React.ComponentProps<typeof Autocomplete>['renderOption']} [props.renderOption]
+ * @param {React.ComponentProps<typeof Autocomplete>['filterOptions']} [props.filterOptions]
+ *    A function that determines the filtered options to be rendered on search.
+ * @param {React.ComponentProps<typeof Autocomplete>['getOptionLabel']} [props.getOptionLabel]
+ *    Used to determine the string value for a given option. It's used to fill
+ *    the input (and the list box options if `renderOption` is not provided). If
+ *    used non-enum mode, it must accept both the type of the options and a
+ *    string.
+ * @param {React.ComponentProps<typeof Autocomplete>['isOptionEqualToValue']} [props.isOptionEqualToValue]
+ *    Used to determine if the option represents the given value. Uses strict
+ *    equality by default. ⚠️ Both arguments need to be handled, an option can
+ *    only match with one value.
+ * @param {React.ComponentProps<typeof Autocomplete<string>>['forcePopupIcon']} [props.forcePopupIcon]
+ * @param {React.ComponentProps<typeof Autocomplete<string>>['noOptionsText']} [props.noOptionsText]
+ * @param {React.ComponentProps<typeof Autocomplete<string>>['renderInput']} [props.renderInput]
+ * @param {React.ComponentProps<typeof Autocomplete<string>>['className']} [props.className]
+ * @param {() => void} [props.fillDefaultFunction]
  */
 export default function DropdownAttribute({
   options,
@@ -34,6 +45,10 @@ export default function DropdownAttribute({
   forcePopupIcon,
   noOptionsText,
   className,
+  getOptionValue = (o) => o,
+  filterOptions,
+  getOptionLabel,
+  isOptionEqualToValue,
   renderInput = (params) => (
     <TextField
       {...params}
@@ -58,6 +73,12 @@ export default function DropdownAttribute({
   const { doc, updateDoc, replaceDoc } = React.useContext(DocumentEditorContext)
   const persistedValue = /** @type {string} */ (value ?? '')
   const [inputValue, setInputValue] = React.useState(persistedValue)
+  const autocompleteValue = isEnum
+    ? !persistedValue
+      ? null
+      : (options.find((option) => getOptionValue(option) === persistedValue) ??
+        null)
+    : value
 
   React.useEffect(() => {
     setInputValue(persistedValue)
@@ -73,16 +94,19 @@ export default function DropdownAttribute({
             disableClearable={disableClearable}
             options={options}
             freeSolo={!isEnum}
-            value={value}
+            value={autocompleteValue}
             onChange={(_, newValue) => {
               updateDoc(
                 props.instancePath,
-                /** @type {string} */ (newValue ?? ''),
+                newValue == null ? '' : getOptionValue(newValue),
               )
             }}
             inputValue={inputValue}
             forcePopupIcon={forcePopupIcon}
             renderOption={renderOption}
+            filterOptions={filterOptions}
+            getOptionLabel={getOptionLabel}
+            isOptionEqualToValue={isOptionEqualToValue}
             noOptionsText={noOptionsText}
             onInputChange={(_, newInputValue) => {
               setInputValue(newInputValue ?? '')
@@ -98,12 +122,16 @@ export default function DropdownAttribute({
                 // option exactly (which means it is an allowed input) and we
                 // don't have this value in the model yet we apply it to the
                 // model ...
-                if (options.includes(inputValue)) {
-                  if (inputValue !== persistedValue) {
+                const matchedOption = options.find(
+                  (o) => getOptionValue(o) === inputValue,
+                )
+                if (matchedOption !== undefined) {
+                  const resolvedValue = getOptionValue(matchedOption)
+                  if (resolvedValue !== persistedValue) {
                     newDoc = updateDocHelper(
                       newDoc,
                       props.instancePath,
-                      inputValue,
+                      resolvedValue,
                     )
                   }
                 } else {
--- app/lib/app/SecvisogramPage/View/FormEditor/editors/GenericEditor/Attributes/IdAttribute/IdAttribute.js
+++ app/lib/app/SecvisogramPage/View/FormEditor/editors/GenericEditor/Attributes/IdAttribute/IdAttribute.js
@@ -1,5 +1,5 @@
 import { TextField } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 import DropdownAttribute from '../DropdownAttribute.js'
 
 /**
@@ -15,15 +15,13 @@ import DropdownAttribute from '../DropdownAttribute.js'
  * }} props
  */
 export default function IdAttribute({ onCollectIds, ...props }) {
-  const [entries, setEntries] = React.useState(new Array())
+  /** @typedef {{ id: string; name: string }} Value */
 
-  const displayIdAndName = (/** @type {string} */ id) => {
-    if (!id) return ''
-    const name = entries.find((w) => w.id === id)?.name
-    return `${id} - ${name}`
-  }
+  const [entries, setEntries] = React.useState(
+    /** @type {Value[]} */ (new Array()),
+  )
 
-  const handleFocus = () => {
+  useEffect(() => {
     if (onCollectIds) {
       onCollectIds().then((entries) => {
         if (entries) {
@@ -31,32 +29,47 @@ export default function IdAttribute({ onCollectIds, ...props }) {
         }
       })
     }
-  }
+  }, [onCollectIds])
 
   return (
     <DropdownAttribute
       {...props}
       className="autocomplete"
       noOptionsText={'No results found'}
-      options={entries.map((entry) => entry.id)}
+      options={entries}
       isEnum={true}
       disabled={false}
       disableClearable={true}
       forcePopupIcon={false}
+      getOptionValue={(option) => /** @type {Value} */ (option).id}
+      // The option label function is used here to determine the value for the
+      // input field once an option was selected.
+      getOptionLabel={(option) => {
+        const o = /** @type {string | Value} */ (option)
+        return typeof o === 'string' ? o : o.id
+      }}
+      isOptionEqualToValue={(option, value) => {
+        const left = /** @type {Value} */ (option)
+        const right = /** @type {string | Value} */ (value)
+        return left.id === (typeof right === 'string' ? right : right.id)
+      }}
+      filterOptions={(options, { inputValue: q }) => {
+        const typedOptions = /** @type {Value[]} */ (options)
+        const lower = q.toLowerCase()
+        return typedOptions.filter(
+          (o) =>
+            o.id.toLowerCase().includes(lower) ||
+            (o.name ?? '').toLowerCase().includes(lower),
+        )
+      }}
       renderInput={(params) => (
-        <TextField
-          {...params}
-          label=""
-          placeholder=""
-          size="small"
-          onFocus={handleFocus}
-        />
+        <TextField {...params} label="" placeholder="" size="small" />
       )}
       renderOption={(props, option) => {
-        const o = /** @type {string} */ (option)
+        const o = /** @type {Value} */ (option)
         return (
-          <li {...props} key={o}>
-            {displayIdAndName(o)}
+          <li {...props} key={o.id}>
+            {o.id} - {o.name}
           </li>
         )
       }}
